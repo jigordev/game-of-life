2e69de75c3eda1486e1f9e153e78cb6f6f5eed94
jigordev <jigordev@gmail.com>
2025-04-28 00:42:25 -0300

    feat: update infection system

--- game.js
+++ game.js
@@ -6,17 +6,19 @@ const rows = canvas.height / cellSize;
 const cols = canvas.width / cellSize;
 
 const minNeighbors = 3; // Mínimo de vizinhos para sobreviver
-const maxNeighbors = 6; // Máximo de vizinhos para morrer de super lotação
+const maxNeighbors = 8; // Máximo de vizinhos para morrer de super lotação
 const reproduction = 3; // Mínimo de vizinhos para se reproduzir em um espaço vazio
 const minFood = 1; // Mínimo de comida para sobreviver sozinho
-const minInfection = 5; // Mínimo de vizinhos para sobreviver a uma infecção
-const spawn = 2 // 2 - Apenas vida, 3 - Vida e comida, 4 - Vida, comida e inimigos
+const minInfection = 6; // Mínimo de vizinhos para sobreviver a uma infecção
+const foodPerTurn = 2; // Número de comida que surge a cada atualização
+const enemyPerTurn = 5; // Número de inimigos que surge a cada atualização
+const initialSpawn = 2 // 2 - Apenas vida, 3 - Vida e comida, 4 - Vida, comida e inimigos
 
 let grid = createGrid();
 
 function createGrid() {
   return Array.from({ length: rows }, () =>
-    Array.from({ length: cols }, () => Math.floor(Math.random() * spawn))
+    Array.from({ length: cols }, () => Math.floor(Math.random() * initialSpawn))
   );
 }
 
@@ -38,28 +40,41 @@ function drawGrid() {
   }
 }
 
+function isGameOver() {
+  let count = 0;
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === 1) {
+        count += 1;
+      }
+    }
+  }
+  return count === 0;
+}
+
 function updateGrid() {
   const newGrid = grid.map(arr => [...arr]);
 
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      const neighbors = countNeighbors(grid, x, y);
+      const neighbors = getNeighbors(grid, x, y);
       const foods = getFood(grid, x, y);
-      const enemies = getEnemy(grid, x, y);
       if (grid[y][x] === 1) {
         if (foods.length > minFood) {
           const idx = Math.floor(Math.random() * foods.length);
           const food = foods[idx];
           newGrid[food.y][food.x] = 0;
-        } else if (neighbors < minNeighbors || neighbors > maxNeighbors) {
+        } else if (neighbors.length < minNeighbors || neighbors.length > maxNeighbors) {
           newGrid[y][x] = 0; // Morre por solidão ou superpopulação
         }
-
-        if ((enemies.length * minInfection) > neighbors) {
-          newGrid[y][x] = 0; // Morre pelo inimigo
+      } else if (grid[y][x] === 3) {
+        if (neighbors.length > 0 && neighbors.length <= minInfection) {
+          neighbors.forEach((n) => {
+            grid[n.y][n.x] = 3;
+          });
         }
       } else {
-        if (neighbors === reproduction) {
+        if (neighbors.length === reproduction) {
           newGrid[y][x] = 1; // Nasce uma nova célula
         }
       }
@@ -67,21 +82,6 @@ function updateGrid() {
   }
 
   grid = newGrid;
-}
-
-function countNeighbors(grid, x, y) {
-  let count = 0;
-  for (let dy = -1; dy <= 1; dy++) {
-    for (let dx = -1; dx <= 1; dx++) {
-      if (dx === 0 && dy === 0) continue;
-      const newY = y + dy;
-      const newX = x + dx;
-      if (newY >= 0 && newY < rows && newX >= 0 && newX < cols) {
-        count += grid[newY][newX] === 1 ? 1 : 0;
-      }
-    }
-  }
-  return count;
 }
 
 function getFood(grid, x, y) {
@@ -101,21 +101,21 @@ function getFood(grid, x, y) {
   return foods;
 }
 
-function getEnemy(grid, x, y) {
-  const enemies = [];
+function getNeighbors(grid, x, y) {
+  const neighbors = [];
   for (let dy = -1; dy <= 1; dy++) {
     for (let dx = -1; dx <= 1; dx++) {
       if (dx === 0 && dy === 0) continue;
       const newY = y + dy;
       const newX = x + dx;
       if (newY >= 0 && newY < rows && newX >= 0 && newX < cols) {
-        if (grid[newY][newX] === 3) {
-          enemies.push({x: newX, y: newY});
+        if (grid[newY][newX] === 1) {
+          neighbors.push({x: newX, y: newY});
         }
       }
     }
   }
-  return enemies;
+  return neighbors;
 }
 
 function spawnFood(count) {
@@ -132,17 +132,21 @@ function spawnEnemy(count) {
   for (let i = 0; i < count; i++) {
     const x = Math.floor(Math.random() * cols);
     const y = Math.floor(Math.random() * rows);
-    if (grid[y][x] === 0 || grid[y][x] === 1) {
+    if (grid[y][x] === 0) {
       grid[y][x] = 3;
     }
   }
 }
 
 function loop() {
-  spawnFood(1);
-  spawnEnemy(3);
+  spawnFood(foodPerTurn);
+  spawnEnemy(enemyPerTurn);
   drawGrid();
   updateGrid();
+  if (isGameOver()) {
+    alert("Game over!");
+    grid = createGrid();
+  }
   requestAnimationFrame(loop);
 }
 
